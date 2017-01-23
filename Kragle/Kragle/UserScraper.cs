af171cc482df3e304b2ee973b8b4fd27bbf346f4
Felix Dekker <felix@fwdekker.com>
2017-01-23 13:13:57 +0100

    Implement basic user scraper

---
+++ Kragle/Kragle/UserScraper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Kragle
+{
+    /// <summary>
+    ///     A scraper that scrapes the list of most recent users.
+    /// </summary>
+    public class UserScraper : Scraper
+    {
+        public UserScraper(bool noCache) : base(noCache)
+        {
+        }
+
+
+        /// <summary>
+        ///     Returns the <code>ICollection</code> of most recently published projects. The results are returned as pages.
+        /// </summary>
+        /// <param name="pageNumber">the number of the page to return; must be at least 0</param>
+        /// <param name="pageSize">the number of projects per page; must be between 1 and 20 (inclusive)</param>
+        /// <returns>the <code>ICollection</code> of most recently published projects</returns>
+        protected ICollection<dynamic> GetRecentProjects(int pageNumber, int pageSize)
+        {
+            if (pageNumber < 0)
+            {
+                throw new ArgumentException("The page number must be at least 0.");
+            }
+            if (pageSize < 1 || pageSize > 20)
+            {
+                throw new ArgumentException("The page size must be between 1 and 20 (inclusive).");
+            }
+
+            // Fetch JSON
+            const string url = "https://api.scratch.mit.edu/search/projects?mode=recent&offset={0}&limit={1}";
+            dynamic projectList = GetJson(string.Format(url, pageNumber, pageSize));
+
+            // Parse to collection
+            ICollection<dynamic> projects = new List<dynamic>(pageSize);
+            foreach (dynamic project in projectList)
+            {
+                projects.Add(project);
+            }
+
+            return projects;
+        }
+    }
+}
---
+++ Kragle/KragleTests/UserScraperTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Kragle;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace KragleTests
+{
+    [TestClass]
+    public class UserScraperTests : UserScraper
+    {
+        public UserScraperTests() : base(false)
+        {
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void UserScraperNegativePageTest()
+        {
+            GetRecentProjects(-3, 14);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void UserScraperExcessiveSizeTest()
+        {
+            GetRecentProjects(28, 94);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void UserScraperInsufficientSizeTest()
+        {
+            GetRecentProjects(12, -38);
+        }
+
+        [TestMethod]
+        public void UserScraperSizeTest()
+        {
+            ICollection<dynamic> projects = GetRecentProjects(98, 14);
+
+            Assert.AreEqual(14, projects.Count);
+        }
+    }
+}
