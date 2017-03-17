b0c541ccc86891ab78aeafb1df454d40ad3db9f2
Felix Dekker <felix@fwdekker.com>
2017-03-17 21:55:08 +0100

    Add Logger class

---
+++ Kragle/Kragle/Logger.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.IO;
+
+
+namespace Kragle
+{
+    /// <summary>
+    ///     A logger that logs to the console output and to a file. Objects of this class should be treated as if they
+    ///     were a stream to the log file, where the <code>Log</code> and <code>LogLine</code> methods incidentally also
+    ///     calll <code>Console.Write</code> or <code>Console.WriteLine</code> methods, respectively.
+    /// </summary>
+    public class Logger : IDisposable
+    {
+        private readonly StreamWriter _output;
+        private bool _autoFlush = true;
+        private bool _console = true;
+
+
+        /// <summary>
+        ///     Constructs a new <code>Logger</code>. This logger only writes to the console output, not to a file.
+        /// </summary>
+        public Logger()
+        {
+            _output = null;
+        }
+
+        /// <summary>
+        ///     Constructs a new <code>Logger</code> that writes to both the console output and the given file.
+        /// </summary>
+        /// <param name="output">the path to the file to write the log to</param>
+        public Logger(string output)
+        {
+            _output = new StreamWriter(output) {AutoFlush = false};
+        }
+
+        /// <summary>
+        ///     The <code>Logger</code> writes to the console output if and only if this value is set to
+        ///     <code>true</code>; calls to <code>Log</code> and <code>LogLine</code> are simply ignored, not
+        ///     buffered.
+        ///     Defaults to <code>true</code>.
+        /// </summary>
+        public bool Console
+        {
+            get { return _console; }
+            set { _console = value; }
+        }
+
+        /// <summary>
+        ///     If set to <code>true</code>, each call to <code>Log</code> or <code>LogLine</code> will also
+        ///     initiate a call to <code>Flush</code>. If set to <code>false</code>, it is guaranteed that the file
+        ///     stream does not automatically flush; the console output stream may still automatically flush depending
+        ///     on its own setting.
+        ///     Setting this field to <code>true</code> will also directly call <code>Flush</code>.
+        ///     Defaults to <code>true</code>.
+        /// </summary>
+        public bool AutoFlush
+        {
+            get { return _autoFlush; }
+            set
+            {
+                _autoFlush = value;
+                if (value)
+                {
+                    Flush();
+                }
+            }
+        }
+
+
+        public void Dispose()
+        {
+            if (_output != null)
+            {
+                _output.Dispose();
+            }
+        }
+
+
+        /// <summary>
+        ///     Logs a string.
+        /// </summary>
+        /// <param name="text">a string</param>
+        /// <returns>this <code>Logger</code></returns>
+        public Logger Log(string text)
+        {
+            if (Console)
+            {
+                System.Console.Write(text);
+            }
+            if (_output != null)
+            {
+                _output.Write(text);
+            }
+
+            if (AutoFlush)
+            {
+                Flush();
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        ///     Logs a string and appends a newline.
+        /// </summary>
+        /// <param name="text">a string</param>
+        /// <returns>this <code>Logger</code></returns>
+        public Logger LogLine(string text)
+        {
+            return Log(text + Environment.NewLine);
+        }
+
+        /// <summary>
+        ///     Flushes the console (if the <code>Console</code> field is set to <code>true</code>) and the file stream
+        ///     (if there is one).
+        /// </summary>
+        /// <returns>this <code>Logger</code></returns>
+        public Logger Flush()
+        {
+            if (Console)
+            {
+                System.Console.Out.Flush();
+            }
+            if (_output != null)
+            {
+                _output.Flush();
+            }
+
+            return this;
+        }
+    }
+}
---
+++ Kragle/KragleTests/LoggerTests.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using Kragle;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace KragleTests
+{
+    [TestClass]
+    public class LoggerTests
+    {
+        private string _filename;
+
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _filename = Path.GetRandomFileName();
+            File.WriteAllText(_filename, "");
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            File.Delete(_filename);
+        }
+
+
+        // Constructor
+        [TestMethod]
+        [SuppressMessage("ReSharper", "UnusedVariable")]
+        public void ConstructorEmptyFileTest()
+        {
+            // Test that calling constructor creates empty file
+            File.Delete(_filename);
+
+            using (Logger logger = new Logger(_filename))
+            {
+            }
+
+            Assert.AreEqual("", File.ReadAllText(_filename));
+        }
+
+        [TestMethod]
+        public void ConstructorConsoleDisabledTest()
+        {
+            using (StringWriter writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+
+                using (Logger logger = new Logger(_filename) {Console = false})
+                {
+                    logger.Log("vjeGzEZvNs");
+                }
+
+                Assert.AreEqual("", writer.ToString());
+            }
+        }
+
+        [TestMethod]
+        public void ConstructorConsoleEnabledTest()
+        {
+            using (StringWriter writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+
+                using (Logger logger = new Logger(_filename))
+                {
+                    logger.Log("bvGS3rh7Wz");
+                }
+
+                Assert.AreEqual("bvGS3rh7Wz", writer.ToString());
+            }
+        }
+
+
+        // Set/Get Console
+        [TestMethod]
+        public void SetGetConsoleTest()
+        {
+            using (Logger logger = new Logger())
+            {
+                logger.Console = true;
+                Assert.AreEqual(true, logger.Console);
+
+                logger.Console = false;
+                Assert.AreEqual(false, logger.Console);
+            }
+        }
+
+        [TestMethod]
+        public void SetConsoleFalseTest()
+        {
+            using (StringWriter writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+
+                using (Logger logger = new Logger(_filename))
+                {
+                    logger.Console = true;
+                    logger.Log("rD1Va0QDrD");
+
+                    logger.Console = false;
+                    logger.Log("l1QMidiEh1");
+                }
+
+                Assert.AreEqual("rD1Va0QDrD", writer.ToString());
+            }
+        }
+
+        [TestMethod]
+        public void SetConsoleTrueTest()
+        {
+            using (StringWriter writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+
+                using (Logger logger = new Logger(_filename))
+                {
+                    logger.Console = false;
+                    logger.Log("BZuzNRpslq");
+
+                    logger.Console = true;
+                    logger.Log("3QJPTPSOLn");
+                }
+
+                Assert.AreEqual("3QJPTPSOLn", writer.ToString());
+            }
+        }
+
+
+        // Set/Get AutoFlush
+        [TestMethod]
+        public void SetGetAutoFlushTest()
+        {
+            using (Logger logger = new Logger())
+            {
+                logger.AutoFlush = true;
+                Assert.AreEqual(true, logger.AutoFlush);
+
+                logger.AutoFlush = false;
+                Assert.AreEqual(false, logger.AutoFlush);
+            }
+        }
+
+        [TestMethod]
+        public void AutoFlushFileTrueTest()
+        {
+            using (FileStream fileIn = new FileStream(_filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (StreamReader reader = new StreamReader(fileIn))
+            using (Logger logger = new Logger(_filename) {AutoFlush = true})
+            {
+                logger.Log("vH2X3NOV8K");
+                Assert.AreEqual("vH2X3NOV8K", reader.ReadToEnd());
+            }
+        }
+
+        [TestMethod]
+        public void AutoFlushFileFalseTest()
+        {
+            using (FileStream fileIn = new FileStream(_filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (StreamReader reader = new StreamReader(fileIn))
+            using (Logger logger = new Logger(_filename) {AutoFlush = false})
+            {
+                logger.Log("67RkBj2AQE");
+                Assert.AreEqual("", reader.ReadToEnd());
+
+                logger.AutoFlush = true;
+                Assert.AreEqual("67RkBj2AQE", reader.ReadToEnd());
+            }
+        }
+
+
+        // Log
+        [TestMethod]
+        public void LogTest()
+        {
+            using (Logger logger = new Logger(_filename))
+            {
+                logger.Log("4HSrOMkg4X");
+            }
+
+            Assert.AreEqual("4HSrOMkg4X", File.ReadAllText(_filename));
+        }
+
+        [TestMethod]
+        public void LogMultipleTest()
+        {
+            using (Logger logger = new Logger(_filename))
+            {
+                logger.Log("6cLJHbijSO");
+                logger.Log("4HSrOMkg4X");
+            }
+
+            Assert.AreEqual("6cLJHbijSO4HSrOMkg4X", File.ReadAllText(_filename));
+        }
+
+        [TestMethod]
+        public void LogChainTest()
+        {
+            using (Logger logger = new Logger(_filename))
+            {
+                logger.Log("NhD7A7VpJF").Log("bVhfBW15Ku");
+            }
+
+            Assert.AreEqual("NhD7A7VpJFbVhfBW15Ku", File.ReadAllText(_filename));
+        }
+
+
+        // LogLine
+        [TestMethod]
+        public void LogLineTest()
+        {
+            using (Logger logger = new Logger(_filename))
+            {
+                logger.LogLine("9453YjrEvB");
+            }
+
+            Assert.AreEqual("9453YjrEvB" + Environment.NewLine, File.ReadAllText(_filename));
+        }
+
+        [TestMethod]
+        public void LogLineMultipleTest()
+        {
+            using (Logger logger = new Logger(_filename))
+            {
+                logger.LogLine("UaWNYl8Bnl");
+                logger.LogLine("eBHy0RksHS");
+            }
+
+            Assert.AreEqual(
+                "UaWNYl8Bnl" + Environment.NewLine + "eBHy0RksHS" + Environment.NewLine,
+                File.ReadAllText(_filename)
+            );
+        }
+
+        [TestMethod]
+        public void LogLineChainTest()
+        {
+            using (Logger logger = new Logger(_filename))
+            {
+                logger.LogLine("Zser78psU5").LogLine("Xlv0ZkP8d1");
+            }
+
+            Assert.AreEqual(
+                "Zser78psU5" + Environment.NewLine + "Xlv0ZkP8d1" + Environment.NewLine,
+                File.ReadAllText(_filename)
+            );
+        }
+
+
+        // Flush
+        [TestMethod]
+        public void FlushConsoleTest()
+        {
+            using (FileStream fileIn = new FileStream(_filename, FileMode.Append, FileAccess.Write, FileShare.Read))
+            using (FileStream fileOut = new FileStream(_filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (StreamWriter writer = new StreamWriter(fileIn) {AutoFlush = false}) // "Console" does not flush
+            using (StreamReader reader = new StreamReader(fileOut))
+            using (Logger logger = new Logger {AutoFlush = false}) // Logger does not flush either
+            {
+                Console.SetOut(writer);
+
+                logger.Log("eb7nplzYBG");
+                Assert.AreEqual("", reader.ReadToEnd());
+
+                logger.Log("EICUpWeTDt").Flush();
+                Assert.AreEqual("eb7nplzYBGEICUpWeTDt", reader.ReadToEnd());
+            }
+        }
+
+        [TestMethod]
+        public void FlushFileTest()
+        {
+            using (FileStream fileIn = new FileStream(_filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (StreamReader reader = new StreamReader(fileIn))
+            using (Logger logger = new Logger(_filename) {AutoFlush = false})
+            {
+                logger.Log("vH2X3NOV8K");
+                Assert.AreEqual("", reader.ReadToEnd());
+
+                logger.Log("N1BpV8sTHJ").Flush();
+                Assert.AreEqual("vH2X3NOV8KN1BpV8sTHJ", reader.ReadToEnd());
+            }
+        }
+    }
+}
