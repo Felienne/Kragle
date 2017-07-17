7dff038261a997edff0ce2f01381c105f44e92e8
Felix Dekker <felix@fwdekker.com>
2017-07-17 10:19:53 +0200

    Make some parts of parsing optional

--- Kragle/Kragle/ConsoleOptions/ParseSubOptions.cs
+++ Kragle/Kragle/ConsoleOptions/ParseSubOptions.cs
@@ -1,10 +1,23 @@
-﻿namespace Kragle.ConsoleOptions
+﻿using CommandLine;
+
+
+namespace Kragle.ConsoleOptions
 {
     /// <summary>
     ///     Command-line options for the 'parse' verb.
     /// </summary>
     public class ParseSubOptions : SubOptions
     {
+        [Option('u', "users", HelpText = "Parse user data")]
+        public bool Users { get; set; }
+
+        [Option('p', "projects", HelpText = "Parse project data")]
+        public bool Projects { get; set; }
+
+        [Option('c', "code", HelpText = "Parse project code")]
+        public bool Code { get; set; }
+
+
         /// <summary>
         ///     Parses users, projects, and project code.
         /// </summary>
@@ -13,9 +26,18 @@
             FileStore.Init(Path);
 
             CodeParser parser = new CodeParser();
-            parser.WriteUsers();
-            parser.WriteProjects();
-            parser.WriteCode();
+            if (Users)
+            {
+                parser.WriteUsers();
+            }
+            if (Projects)
+            {
+                parser.WriteProjects();
+            }
+            if (Code)
+            {
+                parser.WriteCode();
+            }
         }
     }
 }
